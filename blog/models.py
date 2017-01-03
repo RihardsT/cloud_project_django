79303acce80e966591f3c0242a224fe9f6989edb
Rihards Teteris <richitislv@gmail.com>
2017-01-03 09:03:07 +0200

    Add Article model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    article_title_text = models.CharField(max_length=200)
+    article_content = models.TextField()
+    date_published = models.DateTimeField('date published')
