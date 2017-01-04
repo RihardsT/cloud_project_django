a4ce61663a4332cb64d406db3ceb6e2a9182baa8
Rihards Teteris <richitislv@gmail.com>
2017-01-04 23:08:21 +0200

    Add def's to model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,15 @@
+import datetime
+
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
 
 # Create your models here.
 class Article(models.Model):
     article_title_text = models.CharField(max_length=200)
     article_content = models.TextField()
     date_published = models.DateTimeField('date published')
+    def __str__(self):
+        return self.article_content
+    def was_published_recently(self):
+        return self.date_published >= timezone.now() - datetime.timedelta(days=1)
