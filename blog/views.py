3672bf22e28bfa9a9f59388ae161e70f7de8fb3f
Rihards Teteris <richitislv@gmail.com>
2017-01-05 22:48:01 +0200

    Use render in view

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 
 from .models import Article
 
 # Create your views here.
 def index(request):
     latest_article_list = Article.objects.order_by('-date_published')[:5]
-    # output = ', '.join([a.article_title_text for a in latest_article_list])
-    # return HttpResponse(output)
-    template = loader.get_template('blog/index.html')
-    context = {
-        'latest_article_list': latest_article_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_article_list': latest_article_list }
+    return render(request, 'blog/index.html', context)
 
 
 def detail(request, article_id):
