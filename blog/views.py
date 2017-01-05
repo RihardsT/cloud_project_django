642affd9344672e85fda5aa439266928a358d307
Rihards Teteris <richitislv@gmail.com>
2017-01-05 22:45:55 +0200

    Add a template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Article
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    latest_article_list = Article.objects.order_by('-date_published')[:5]
+    # output = ', '.join([a.article_title_text for a in latest_article_list])
+    # return HttpResponse(output)
+    template = loader.get_template('blog/index.html')
+    context = {
+        'latest_article_list': latest_article_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, article_id):
+    return HttpResponse("You're looking at article %s." % article_id)
