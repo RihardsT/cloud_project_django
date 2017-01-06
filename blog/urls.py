a962344f097ebc907ddeb03392181f66f5a9332c
Rihards Teteris <richitislv@gmail.com>
2017-01-06 23:39:20 +0200

    Add likes to article. Dirty.
    Don't like this much. Differs too much from tutorial, but eh.
    Will have to clean this up later.
    Now just learning, so doesn't matter that much

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<article_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<article_id>[0-9]+)/like/$', views.like, name='like'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Article
 
@@ -13,3 +14,22 @@ def index(request):
 def detail(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
     return render(request, 'blog/detail.html', {'article': article})
+
+def like(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    try:
+        article.likes += 1
+        article.save()
+    except (KeyError, Article.DoesNotExist):
+        # Redisplay the article like form.
+        return render(request, 'blog/detail.html', {
+            'article': article,
+            'error_message': "You didn't select like.",
+        })
+    else:
+        # article.likes += 1
+        # article.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('blog:detail', args=(article.id,)))
