c78eba6758ff9d20d6ea22c8b64300c25725febc
Rihards Teteris <richitislv@gmail.com>
2017-01-09 08:24:52 +0200

    Modify admin page

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import Article
 
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['article_title_text', 'article_content']}),
+        ('Date information', {'fields': ['date_published'], 'classes': ['collapse']}),
+    ]
+    # fields = ['date_published', 'article_title_text', 'article_content']
+    list_display = ('article_title_text', 'date_published', 'was_published_recently')
+    list_filter = ['date_published']
+    search_fields = ['article_title_text']
+
+admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,6 @@ class Article(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.date_published <= now
+    was_published_recently.admin_order_field = 'date_published'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
