3a3defa67adf1ff8375ac6524c32e2409e758232
Rihards Teteris <richitislv@gmail.com>
2017-02-26 20:20:01 +0200

    Create welcome table in db
    This will be used to display/change general info about section
    This will be incredibly hard to maintain, if I ever set this in subpages

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Welcome(models.Model):
+    welcome_text = models.TextField()
+    display_on_page = models.CharField(max_length=200)
+    def __str__(self):
+        return self.welcome_text
