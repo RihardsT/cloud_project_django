fe8e9501ffe24abf69d26231dba36883f52be78a
Rihards Teteris <richitislv@gmail.com>
2017-02-27 21:52:51 +0200

    Add possible page description from DB
    Welcome should be renamed to show that it's meant to be page description
    And none of this seems particularly good approach for this

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Welcome
+
+class WelcomeAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Welcome, WelcomeAdmin)
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 from blog.models import Article
+from main.models import Welcome
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -9,4 +10,5 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
         context['latest_article'] = Article.objects.last()
+        context['page_description'] = Welcome.objects.filter(display_on_page="/").first()
         return context
